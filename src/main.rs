7b64a076cff90964a5bd62ea7b4c43a5d087e773
Pauli Jaakkola <pauli.jaakkola@iki.fi>
2017-02-24 21:06:40 +0200

    Get VM up to factorial power.

--- src/main.rs
+++ src/main.rs
@@ -5,7 +5,6 @@ extern crate lalrpop_util as __lalrpop_util;
 
 use std::fs::File;
 use std::io::Read;
-use std::mem::size_of;
 use rustyline::error::ReadlineError;
 use rustyline::Editor;
 
@@ -21,18 +20,57 @@ pub mod vm;
 use util::ProffError;
 use lexer::Lexer;
 use value::RawRef;
-use vm::{VM, Closure, Instr};
+use vm::{VM, CodeObject};
 use vm::Operand::*;
 use vm::Instr::*;
 
+// TODO: tail recursive version
+// fact:
+//     ilt  l1, c0    ; n < 2
+//     br   1         ; jump over ret
+//     ret  c1        ; return 1
+//     isub 2, l1, c1 ; a = n - 1
+//     mov  4, l0     ; f = fact
+//     mov  0, l1     ; save n
+//     mov  5, l2     ; arg[0] = a
+//     call 4         ; b = fact (n - 1)
+//     imul 2, l0, l1 ; c = n * fact (n - 1)
+//     ret  l2        ; return c
+
+// main:
+//     fun  3, 0      ; f = new fact()
+//     mov  4, c0     ; arg0 = 5
+//     call 3
+
 fn main() {
-    let mut vm = VM::new(Closure {
+    let mut vm = VM::new(CodeObject {
         code: vec![
-            Regs(2),
-            IAdd(0, From::from(Const(0)), From::from(Const(1))),
-            IMul(1, From::from(Local(0)), From::from(Const(0))),
-            Halt],
-        consts: vec![RawRef(3), RawRef(5)]
+            Fun(3, 0),
+            Mov(4, From::from(Const(0))),
+            Call(3),
+            Halt
+        ],
+        consts: vec![RawRef(5)],
+        reg_req: 5,
+        cobs: vec![
+            CodeObject {
+                code: vec![
+                    ILt(From::from(Local(1)), From::from(Const(0))),
+                    Br(1),
+                    Ret(From::from(Const(1))),
+                    ISub(2, From::from(Local(1)), From::from(Const(1))),
+                    Mov(4, From::from(Local(0))),
+                    Mov(0, From::from(Local(1))),
+                    Mov(5, From::from(Local(2))),
+                    Call(4),
+                    IMul(2, From::from(Local(0)), From::from(Local(1))),
+                    Ret(From::from(Local(2)))
+                ],
+                consts: vec![RawRef(2), RawRef(1)],
+                reg_req: 6,
+                cobs: vec![]
+            }
+        ]
     });
     vm.run();
 
--- src/vm.rs
+++ src/vm.rs
@@ -1,5 +1,6 @@
 use std::fmt;
 use std::fmt::Display;
+use std::mem;
 
 use value::RawRef;
 
@@ -51,10 +52,19 @@ impl From<Operand> for u8 {
 /// A virtual instruction.
 #[derive(Debug, Clone, Copy)]
 pub enum Instr {
-    Regs(u8),
+    Mov(u8, u8),
+
+    Fun(u8, u16),
 
     IAdd(u8, u8, u8),
+    ISub(u8, u8, u8),
     IMul(u8, u8, u8),
+
+    ILt(u8, u8),
+
+    Br(u16),
+    Call(u16),
+    Ret(u8),
 
     Halt
 }
@@ -63,25 +73,39 @@ impl Display for Instr {
     fn fmt(&self, f: &mut fmt::Formatter) -> Result<(), fmt::Error> {
         use self::Instr::*;
         match self {
-            &Regs(n) => write!(f, "regs {}", n),
+            &Mov(dest, src) => write!(f, "mov  l{}, {}", dest, Operand::from(src)),
+            &Fun(d, i) => write!(f, "fun  l{}, {}", d, i),
             &IAdd(d, l, r) => write!(f, "iadd l{}, {}, {}", d, Operand::from(l), Operand::from(r)),
+            &ISub(d, l, r) => write!(f, "isub l{}, {}, {}", d, Operand::from(l), Operand::from(r)),
             &IMul(d, l, r) => write!(f, "imul l{}, {}, {}", d, Operand::from(l), Operand::from(r)),
+            &ILt(l, r) => write!(f, "ilt  {}, {}", Operand::from(l), Operand::from(r)),
+            &Br(offset) => write!(f, "br   {}", offset),
+            &Call(offset) => write!(f, "call {}", offset),
+            &Ret(v) => write!(f, "ret  {}", Operand::from(v)),
             &Halt => write!(f, "halt")
         }
     }
 }
 
+/// A temporary shim for code objects
+#[derive(Debug, Clone)]
+pub struct CodeObject {
+    pub code: Vec<Instr>,
+    pub consts: Vec<RawRef>,
+    pub reg_req: usize,
+    pub cobs: Vec<CodeObject>
+}
+
 /// A temporary shim for closures.
 #[derive(Debug)]
 pub struct Closure {
-    pub code: Vec<Instr>,
-    pub consts: Vec<RawRef>
+    pub cob: CodeObject
 }
 
 /// Proff virtual machine
 #[derive(Debug)]
 pub struct VM {
-    cl: Closure,
+    cl: Box<Closure>,
     ip: usize,
     fp: usize,
     stack: Vec<RawRef>
@@ -89,12 +113,13 @@ pub struct VM {
 
 impl VM {
     /// Create a new VM.
-    pub fn new(fun: Closure) -> VM {
+    pub fn new(fun: CodeObject) -> VM {
+        let stacksize = fun.reg_req;
         VM {
-            cl: fun,
+            cl: Box::new(Closure { cob: fun }),
             ip: 0,
             fp: 0,
-            stack: Vec::new()
+            stack: vec![RawRef(0); stacksize]
         }
     }
 
@@ -103,14 +128,19 @@ impl VM {
         use self::Instr::*;
 
         loop {
-            let instr = self.cl.code[self.ip];
-            println!("{}: {}", self.ip, instr);
+            let instr = self.cl.cob.code[self.ip];
+            println!("{} [{}]: {}", self.stack.len(), self.ip, instr);
             self.ip += 1;
 
             match instr {
-                Regs(n) => {
-                    let slen = self.stack.len();
-                    self.stack.resize(slen + n as usize, RawRef(0));
+                Mov(di, si) => {
+                    let s = self.decode_operand(si);
+                    self.set_reg(di, s);
+                },
+
+                Fun(di, ci) => {
+                    let cob = self.cl.cob.cobs[ci as usize].clone();
+                    self.set_reg(di, unsafe { mem::transmute(Box::new(Closure {cob: cob })) });
                 },
 
                 IAdd(di, li, ri) => {
@@ -118,10 +148,49 @@ impl VM {
                     let r = self.decode_operand(ri);
                     self.set_reg(di, RawRef(l.0 + r.0));
                 },
+                ISub(di, li, ri) => {
+                    let l = self.decode_operand(li);
+                    let r = self.decode_operand(ri);
+                    self.set_reg(di, RawRef(l.0 - r.0));
+                },
                 IMul(di, li, ri) => {
                     let l = self.decode_operand(li);
                     let r = self.decode_operand(ri);
                     self.set_reg(di, RawRef(l.0 * r.0));
+                },
+
+                ILt(li, ri) => {
+                    let l = self.decode_operand(li);
+                    let r = self.decode_operand(ri);
+                    if l.0 < r.0 {
+                        self.ip += 1;
+                    }
+                },
+
+                Br(offset) => {
+                    self.ip += offset as usize;
+                },
+                // TODO: `Call` is a bit messy. Also need `TCall`. Maybe `Call` can be broken into
+                // `PushK` and `TCall` to solve both?
+                Call(fp_offset) => {
+                    let newfp = self.fp + fp_offset as usize;
+                    self.stack[newfp - 3] = RawRef(self.fp);
+                    self.stack[newfp - 2] = RawRef(self.ip);
+                    let mut cl = unsafe { mem::transmute(self.stack[newfp]) };
+                    mem::swap(&mut cl, &mut self.cl);
+                    self.stack[newfp - 1] = unsafe { mem::transmute(cl) };
+                    self.ip = 0;
+                    self.fp = newfp;
+                    self.stack.resize(self.fp + self.cl.cob.reg_req, RawRef(0));
+                },
+                // TODO: Estimate register quantity better, null out registers above return value.
+                Ret(vi) => {
+                    let newfp = self.stack[self.fp - 3];
+                    self.stack[self.fp - 3] = self.decode_operand(vi);
+                    self.ip = self.stack[self.fp - 2].0;
+                    self.cl = unsafe { mem::transmute(self.stack[self.fp - 1]) };
+                    self.fp = newfp.0;
+                    self.stack.resize(self.fp + self.cl.cob.reg_req, RawRef(0));
                 },
 
                 Halt => break
@@ -132,7 +201,7 @@ impl VM {
     fn decode_operand(&self, operand: u8) -> RawRef {
         match From::from(operand) {
             Operand::Local(li) => self.stack[self.fp + li as usize],
-            Operand::Const(ci) => self.cl.consts[ci as usize]
+            Operand::Const(ci) => self.cl.cob.consts[ci as usize]
         }
     }
 
