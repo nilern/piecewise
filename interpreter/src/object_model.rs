3e98a98c839eb4eb743c406847aeff938245d127
Pauli Jaakkola <pauli.jaakkola@iki.fi>
2017-12-22 22:30:24 +0200

    ValueView and some related Deref impls.

--- interpreter/src/object_model.rs
+++ interpreter/src/object_model.rs
@@ -1,24 +1,33 @@
 use std::ptr::Shared;
 use std::slice;
-use std::mem::size_of;
+use std::mem::{size_of, transmute};
 use std::ops::Deref;
+use std::fmt::{self, Debug, Formatter};
 
 use gce::util::CeilDiv;
 use gce::{Object, ObjectRef, PointyObjectRef};
 use gce::layout::{Granule, GSize};
 
+// ================================================================================================
+
 const TAG_MASK: usize = 0b111;
 const PTR_BIT: usize = 0b001;
 
-#[derive(Clone, Copy)]
-struct GcPtr<T>(*mut T);
+// ================================================================================================
 
-impl<T> Deref for GcPtr<T> {
-    type Target = T;
+#[derive(Debug)]
+pub enum ValueView {
+    Type(GcPtr<Type>),
 
-    fn deref(&self) -> &T { unsafe{ &*self.0 } }
+    Int(isize),
+    Float(f64),
+    Char(char),
+    Bool(bool)
 }
 
+// ================================================================================================
+
+#[derive(Debug)]
 #[repr(C)]
 pub struct HeapValue {
     link: ValueRef,
@@ -27,12 +36,17 @@ pub struct HeapValue {
 
 impl HeapValue {
     fn ref_len(&self) -> usize { self.typ.ref_len }
+
+    fn view(&self) -> ValueView { unimplemented!() }
 }
 
 impl Object for HeapValue {
     fn gsize(&self) -> GSize { From::from(self.typ.size.ceil_div(size_of::<Granule>())) }
 }
 
+// ================================================================================================
+
+#[derive(Debug)]
 #[repr(C)]
 pub struct Type {
     heap_value: HeapValue,
@@ -40,18 +54,45 @@ pub struct Type {
     ref_len: usize
 }
 
+// ================================================================================================
+
+#[derive(Clone, Copy, Debug)]
+pub struct GcPtr<T>(*mut T);
+
+impl<T> Deref for GcPtr<T> {
+    type Target = T;
+
+    fn deref(&self) -> &T { unsafe{ &*self.0 } }
+}
+
+// ================================================================================================
+
 #[derive(Clone, Copy)]
 pub struct ValueRef(usize);
 
 impl ValueRef {
     fn is_ptr(self) -> bool { self.0 & PTR_BIT == 1 }
+
+    fn view(self) -> ValueView {
+        if let Some(sptr) = self.ptr() {
+            unsafe { &*sptr.as_ptr() }.view()
+        } else {
+            match self.0 & TAG_MASK {
+                0b000 => ValueView::Int((self.0 & !TAG_MASK) as isize),
+                0b010 => ValueView::Float((self.0 & !TAG_MASK) as f64),
+                0b100 => ValueView::Char(unsafe { transmute((self.0 & !TAG_MASK) as u32) }),
+                0b110 => ValueView::Bool((self.0 & !TAG_MASK) == 1),
+                _ => unreachable!()
+            }
+        }
+    }
 }
 
 impl ObjectRef for ValueRef {
     type Obj = HeapValue;
     type PORef = PointyValueRef;
 
-    fn ptr(self) -> Option<Shared<Self::Obj>> {
+    fn ptr(self) -> Option<Shared<HeapValue>> {
         if self.is_ptr() {
             Shared::new((self.0 & !TAG_MASK) as _)
         } else {
@@ -59,7 +100,7 @@ impl ObjectRef for ValueRef {
         }
     }
 
-    fn pointy_ref(self) -> Option<Self::PORef> {
+    fn pointy_ref(self) -> Option<PointyValueRef> {
         if (self.0 & TAG_MASK) == 0b011 {
             Some(PointyValueRef(self))
         } else {
@@ -67,6 +108,12 @@ impl ObjectRef for ValueRef {
         }
     }
 }
+
+impl Debug for ValueRef {
+    fn fmt(&self, f: &mut Formatter) -> Result<(), fmt::Error> { self.view().fmt(f) }
+}
+
+// ================================================================================================
 
 #[derive(Clone, Copy)]
 pub struct PointyValueRef(ValueRef);
