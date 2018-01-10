38adaf992cdb2f2bde12843085294742af3c9c01
Pauli Jaakkola <pauli.jaakkola@iki.fi>
2018-01-10 17:26:37 +0200

    Remove compiler-rkt.

--- compiler-rkt/piecewise/info.rkt
+++
@@ -1,6 +0,0 @@
-#lang info
-(define collection "piecewise")
-(define deps '("base" "nanopass"))
-(define pkg-desc "Dynamic functional language")
-(define version "0.0")
-(define pkg-authors '(nilern))
